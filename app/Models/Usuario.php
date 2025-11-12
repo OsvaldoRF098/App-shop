eca3a7c4ef9b2987c98483b43ffb6f2181db0b93
OsvaldoRF098 <osvaldoramirezflores098@gmail.com>
2025-11-12 08:33:01 -0600

    Connect Algolia

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Usuario extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $table = 'usuarios';
 
